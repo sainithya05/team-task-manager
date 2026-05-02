c04e9687be57276ee54d2ee4e0911163a2308943
AADI NITHYA <aadinithya@AADIs-MacBook-Air.local>
2026-05-02 15:46:48 +0530

    hardcode production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://team-task-manager-production-9535.up.railway.app/api',
 });
 
 api.interceptors.request.use((config) => {
